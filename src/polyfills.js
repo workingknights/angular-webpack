b6fc6bf3fc88a279260f94cc4a0127dea9d976d9
workingknights <anthony.knights@gmail.com>
2017-02-23 22:31:43 +0900

    initial commit

---
+++ src/polyfills.js
@@ -0,0 +1,11 @@
+"use strict";
+require("core-js/es6");
+require("core-js/es7/reflect");
+require('zone.js/dist/zone');
+if (process.env.ENV === 'production') {
+}
+else {
+    Error['stackTraceLimit'] = Infinity;
+    require('zone.js/dist/long-stack-trace-zone');
+}
+//# sourceMappingURL=polyfills.js.map
---
+++ src/vendor.js
@@ -0,0 +1,9 @@
+"use strict";
+require("@angular/platform-browser");
+require("@angular/platform-browser-dynamic");
+require("@angular/core");
+require("@angular/common");
+require("@angular/http");
+require("@angular/router");
+require("rxjs");
+//# sourceMappingURL=vendor.js.map
